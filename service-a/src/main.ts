eba38b2f213418f5bbca784629ee65e827aad30a
Carlos Domínguez <carlos.cfdr86@gmail.com>
2025-03-25 15:55:00 -0600

    Se crea archivo swagger para la documentación del service-a

--- service-a/src/main.ts
+++ service-a/src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-      .setTitle('User Service')
-      .setDescription('API para la creación de usuarios')
-      .setVersion('1.0')
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-
+  const document = yaml.load(
+    readFileSync(resolve(__dirname, '../src/docs/swagger.yaml'), 'utf8')
+  );
+  // const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
